6393a5ff7826eb11dff47a466388c8ca70d7edf4
Eder Lara Trujillo <ederlara@misena.edu.co>
2020-10-07 17:04:14 -0500

    Actualización Tarea PHP

--- phpacademy/index.php
+++ phpacademy/index.php
@@ -39,4 +39,10 @@
 	}
 
 
+	# Tarea # 1:
+	# 1. Consultar de acuerdo a la guia de aprendizaje qué son:
+	# 	TEMA 3: Estructuras condicionales y operadores lógicos 
+	# 	TEMA 4: Estructuras repetitivas o cíclicas(bucles: while, do while, for) 
+	# 	TEMA 5: Funciones numéricas, de String, de fecha y hora, include y require 
+
 ?>
