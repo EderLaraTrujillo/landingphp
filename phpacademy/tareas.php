370205b03fec1ef0f721ff03cdf72fc1a0574908
Eder Lara Trujillo <ederlara@misena.edu.co>
2020-10-15 00:17:22 -0500

    Tarea: Métodos Mágicos

--- phpacademy/tareas.php
+++ phpacademy/tareas.php
@@ -13,4 +13,8 @@
 	# $_POST[], $_GET[], $_SESSION[]
 	# qué es json
 
+# Métodos "Mágicos" (se usan con el término function):
+	# __construct (), __destruct(), __get, __call(), __callStatic(), __set(), __isset(), __unset(), __sleep(), __wakeup(), __toString(), __invoke(), __set_state(), __clone()
+	# se pueden ver en este manual de referencia:  https://www.php.net/manual/es/language.oop5.magic.php
+
 ?>
